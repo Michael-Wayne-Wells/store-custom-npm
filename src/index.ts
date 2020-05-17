5d7033e65d9c3ff5bf3c1c0e989586ff651c6655
Michael Wells <mwells1286@gmail.com>
2020-05-17 12:08:26 -0700

    Updates

--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,9 @@ export * from './middlewars/current-user';
 export * from './middlewars/error-handler';
 export * from './middlewars/require-auth';
 export * from './middlewars/validate-request';
+
+export * from './events/base-listener';
+export * from './events/base-publisher';
+export * from './events/product-created-event';
+export * from './events/product-updated-event';
+export * from './events/subjects';
